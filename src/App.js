1c1c6d1254d2043f460d27d23fbda68317c396e9
yadavsatyapal1999 <114231518+yadavsatyapal1999@users.noreply.github.com>
2023-08-10 15:20:01 +0530

    Unnessary data removed from app.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 
 import './App.css';
 
-import BasicDetail from './componants/Add_property/BasicDetail';
-import PropertyDetail from './componants/Add_property/PropertyDetail';
-import General_info from './componants/Add_property/General_info';
-import Location from './componants/Add_property/Location_info';
 
 function App() {
   return (
     <div className="App">
-    <Location/>
+   
     </div>
   );
 }

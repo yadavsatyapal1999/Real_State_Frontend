47cbd7df8f43789677609ccfb06c38f40d94d732
Mehul Sahu <mehulyashams@gmail.com>
2023-08-11 20:11:30 +0530

    nav,sidebar,homepage added,style&desktop folder ad

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import BasicDetail from "../src/componants/Add_property/BasicDetail";
+import Nav from "../src/componants/Nav" 
 import './App.css';
+import { Home } from "./DesktopPages/Home";
+import SideBar from "./componants/Sidebar";
 
 
 function App() {
   return (
     <div >
+    <Home/>
     </div>
   );
 }

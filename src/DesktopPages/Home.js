ff91085b489c8c14db8d249236e5d144e88b36f3
Mehul Sahu <mehulyashams@gmail.com>
2023-08-17 13:51:27 +0530

    render link on home

--- src/DesktopPages/Home.js
+++ src/DesktopPages/Home.js
@@ -93,7 +93,7 @@ const HomePage = () => {
     let token = localStorage.getItem("token");
     axios
         .patch(
-            `http://localhost:8080/prop/v1/sold/${details._id}`,
+            `https://real-state-backend-6416.onrender.com/prop/v1/sold/${details._id}`,
             data,
             {
                 headers: {
